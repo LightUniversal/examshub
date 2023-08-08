15dc76da2d3ab41e3c64625feddc12f43c4ac93f
Lu <87399308+LightUniversal@users.noreply.github.com>
2023-08-08 14:19:24 +0100

    Update OrderScreen.js

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -106,7 +106,7 @@ const OrderScreen = () => {
       <Row>
         <Col md={8}>
           <ListGroup>
-            <ListGroup.Item>
+            <ListGroup.Item className="my-2">
               <p className="px-2 border-bottom">
                 <h6>Department: {order.shippingAddress.department}</h6>
                 <h6>Location: {order.shippingAddress.city}</h6>
@@ -126,8 +126,8 @@ const OrderScreen = () => {
                 </p>
               )}
             </ListGroup.Item>
-            <ListGroup.Item></ListGroup.Item>
-            <ListGroup.Item>
+    
+            <ListGroup.Item className="my-2">
               <h2>Payment Method</h2>
               <p>
                 <strong>Method:</strong>
@@ -139,7 +139,7 @@ const OrderScreen = () => {
                 <Message variant="danger">Not Paid</Message>
               )}
             </ListGroup.Item>
-            <ListGroup.Item>
+            <ListGroup.Item className="my-2">
               <h2>Order Items</h2>
               {order.orderItems.map((item, i) => (
                 <ListGroup.Item key={i}>
